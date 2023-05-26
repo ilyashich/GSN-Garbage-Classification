afae1bc58d59f33527b7c58a95bdf2261b1e8e9b
ilyashich <ilyashich99@gmail.com>
2023-05-26 20:36:15 +0200

    modified augmentation vidualization

--- src/utils/visualize_augmentations.py
+++ src/utils/visualize_augmentations.py
@@ -4,15 +4,25 @@ import matplotlib.pyplot as plt
 import albumentations as A
 from albumentations.pytorch import ToTensorV2
 
+from src.data.trashnet_datamodule import TrashNetDataModule
 
-def visualize_augmentations(dataset, idx=0, samples=10, cols=5):
-    dataset = copy.deepcopy(dataset)
+def visualize_train_augmentations(dataset, num_imgs=10, samples=10, cols=5):
     dataset.transform = A.Compose([t for t in dataset.transform if not isinstance(t, (A.Normalize, ToTensorV2))])
-    rows = samples // cols
-    figure, ax = plt.subplots(nrows=rows, ncols=cols, figsize=(12, 6))
-    for i in range(samples):
-        image, _ = dataset[idx]
-        ax.ravel()[i].imshow(image)
-        ax.ravel()[i].set_axis_off()
-    plt.tight_layout()
-    plt.show()
+    
+    for idx in range(num_imgs):
+        rows = samples // cols
+        figure, ax = plt.subplots(nrows=rows, ncols=cols, figsize=(12, 6))
+        for i in range(samples):
+            image, _ = dataset[idx]
+            ax.ravel()[i].imshow(image)
+            ax.ravel()[i].set_axis_off()
+        plt.tight_layout()
+        plt.show()
+
+if __name__ == "__main__":
+    data_module = TrashNetDataModule()
+    data_module.prepare_data()
+    data_module.setup()
+
+    visualize_train_augmentations(dataset=data_module.dataset_train)
+    
